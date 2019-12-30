b4735ecea594d1c81bd3ba4b155860e964d0f7fa
RobenLC <chenlihome@gmail.com>
2019-12-30 23:13:34 +0800

    fix indentation error and refactor print loop

--- src/helper.c
+++ src/helper.c
@@ -37,8 +37,8 @@ int readfile(char *filename, uint8_t **data)
     ret = fseek(fin, 0, SEEK_END);
     if(ret) {
         printf("input seek end failed ret: %d\n", ret);
-    fclose(fin);
-    return -2;
+        fclose(fin);
+        return -2;
     }
 
     len = ftell(fin);
@@ -47,15 +47,15 @@ int readfile(char *filename, uint8_t **data)
     datain = malloc(len);
     if (!datain) {
         printf("memory allocate failed size: %d \n", len);
-    fclose(fin);
-    return -3;
+        fclose(fin);
+        return -3;
     }
     
     ret = fseek(fin, 0, SEEK_SET);
     if(ret) {
         printf("input seek begin failed ret: %d\n", ret);
-    fclose(fin);
-    return -4;
+        fclose(fin);
+        return -4;
     }
 
     ret = fread(datain, 1, len, fin);
@@ -70,7 +70,7 @@ int readfile(char *filename, uint8_t **data)
 
 int print2file(char *filename, struct data_linklist_s *max32list, int size, uint16_t *last32)
 {
-    int ix=0, len=0, cnt=0, rst=0;
+    int ix=0, len=0, cnt=0, rst=0, base=0;
     FILE *fout=0;
     char str[128]={0};
     struct data_linklist_s *tmplist=0;
@@ -99,19 +99,18 @@ int print2file(char *filename, struct data_linklist_s *max32list, int size, uint
     len = strlen(str);
     fwrite(str, 1, len, fout);
 
-    if (cnt < 32) {
-        for (ix=0; ix < cnt; ix++) {
-            sprintf(str,"%d\n", last32[ix]);
-            len = strlen(str);
-            fwrite(str, 1, len, fout);
-        }
+    rst = cnt % 32;
+
+    if (cnt > 32) {
+        base = 32;
     } else {
-        rst = cnt % 32;
-        for (ix=0; ix < 32; ix++) {
-            sprintf(str,"%d\n", last32[(ix+rst)%32]);
-            len = strlen(str);
-            fwrite(str, 1, len, fout);
-        }
+        base = cnt;
+    }
+
+    for (ix=0; ix < base; ix++) {
+        sprintf(str,"%d\n", last32[(ix+rst)%base]);
+        len = strlen(str);
+        fwrite(str, 1, len, fout);
     }
 
     return 0;
